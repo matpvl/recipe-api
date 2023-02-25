5338574ccdbc26afd953069fb177540570242bfe
Matija <mpavlovi6@gmail.com>
2023-02-25 17:41:17 +0100

    added custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,37 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        # We defaulted the password to None ^^^
+        user.set_password(password)
+        # using=self._db is the best practice since it can support that we add multiple databases
+        # to our project, we always use it when creating or saving a new object using UserManager
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    # We override the username field with email
+    USERNAME_FIELD = 'email'
