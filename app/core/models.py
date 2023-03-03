481bf867a176956989e8dc0fa354aaaee6149a8a
Matija <mpavlovi6@gmail.com>
2023-03-03 01:48:40 +0100

    created Recipe model, migrations, and Recipe app

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -52,3 +53,24 @@ class User(AbstractBaseUser, PermissionsMixin):
     def get_name(self):
         """Get users name field"""
         return self.name.capitalize()
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+    user = models.ForeignKey(
+        # We reference the user model from the settings,
+        # in case something is changed we don't have to go through
+        # mountains of hardcode.
+        settings.AUTH_USER_MODEL,
+        # If the reference object gets deleted, it will also
+        # delete all the recipes associated to the User
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,9 +2,12 @@
 Tests for models.
 """
 import datetime
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -63,3 +66,20 @@ class ModelTests(TestCase):
             email="email@example.com", password="example123", birthday=dob
         )
         self.assertGreaterEqual(date, user.birthday)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        # First we create a test user that we will assign to the recipe.
+        user = get_user_model().objects.create_user(
+            email='test@example.com',
+            password='testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
